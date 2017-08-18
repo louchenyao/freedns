ead299c8c75ef8c6b7ecdfe7ca144374ca7eeafb
Chenyao2333 <louchenyao@gmail.com>
2017-08-19 03:13:57 +0800

    stash

--- index.js
+++ index.js
@@ -31,57 +31,68 @@ let quest_google = function(que, callback) {
         });
         res.on("end", () => {
             let ret = JSON.parse(buf.toString("utf8"));
-            let answers = ret.get("answer", []);
-            update_cache(answers);
-            callback(answers);
+            let answers = ret.get("Answer", []);
+            let authoritys = ret.get("Authority", []);
+            update_cache(answers, "answer");
+            update_cache(authoritys, "authorith");
+            callback(answers, authoritys);
         });
     });
 }
 
 let quest = function (questions, callback) {
     let answers = [];
+    let authoritys = [];
     
     (function (questions) {
         if (!questions) {
-            callback(answers);
+            callback(answers, authoritys);
             return;
         }
 
         x = questions[0];
         questions = questions.slice(1);
 
-        for (let x of questions) {
-            quest_cache(x, (ans, hit) => {
-                if (hit) {
-                    answers.concat(ans);
+        quest_cache(x, (ans, auth, hit) => {
+            if (hit) {
+                answers.concat(ans);
+                authoritys.concat(auth);
+                this.call(questions);
+            } else {
+                quest_google(x, (ans, auth) => {
+                    if (ans) {
+                        answers.concat(ans);
+                        authoritys.concat(auth);
+                        update_cache(ans);
+                    }
                     this.call(questions);
-                } else {
-                    quest_google(x, ans => {
-                        if (ans) {
-                            answers.concat(ans);
-                            update_cache(ans);
-                        }
-                        this.call(questions);
-                    });
-                }
-            });
-        }
+                });
+            }
+        });
     }) (questions);
 }
 
 let to_native_dns_answers = function(google_answers) {
-    let ret = []
+    let ret = [];
     for (let x of google_answers) {
+        let item = {name: x["name"], ttl: x["TTL"], address: x["data"], class: x["type"]};
         if (x.type == 1) {
-            ret.push()
+            ret.push(dns.A(item));
+        } else if (x.type == 28) {
+            ret.push(dns.AAAA(item));
+        } else if (x.type == 6) {
+            ret.push(dns.CNAME(item));
+        } else if (x.type == 15) {
+            ret.push(dns.MX(item));
         }
     }
 }
 
 server.on("request", (req, res) => {
     // console.log(req);
-    quest(req.questions, (answers) => {
+    quest(req.questions, (answers, authoritys) => {
         res.answer = to_native_dns_answers(answers);
+        res.authority = to_native_dns_answers(authoritys);
         res.send();
     });
 });
